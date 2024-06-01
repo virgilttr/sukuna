01d496e73be4e5eb77070366b35a3d4ac62ea89d
virgil <virgil@Virgils-MBP.cable.rcn.com>
2024-06-01 13:53:05 -0400

    Initial commit

---
+++ test/sukuna.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Sukuna from '../lib/sukuna-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sukuna-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Sukuna.SukunaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
