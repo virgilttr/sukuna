a432317cffa653ca16013e80fb689b548440978a
virgil <virgil@virgils-mbp.mynetworksettings.com>
2024-06-22 12:04:41 -0400

    fix bug

--- frontend/components/uploader.tsx
+++ frontend/components/uploader.tsx
@@ -14,13 +14,32 @@ const FileUpload: React.FC = () => {
   };
 
   const uploadFiles = async () => {
-    // ... (rest of the uploadFiles function remains the same)
+    const uploadProgress: { [key: string]: number } = {};
+    files.forEach(file => {
+      uploadProgress[file.name] = 0;
+    });
+    setProgress(uploadProgress);
+    const promises = files.map(file => uploadFile(file));
+    await Promise.all(promises);
+    setUploadComplete(true);
   };
 
   const uploadFile = async (file: File) => {
-    // ... (rest of the uploadFile function remains the same)
+    const formData = new FormData();
+    formData.append('file', file);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/upload', true);
+    xhr.upload.onprogress = (event) => {
+      if (event.lengthComputable) {
+        setProgress(prevProgress => ({
+          ...prevProgress,
+          [file.name]: Math.round((100 * event.loaded) / event.total),
+        }));
+      }
+    };
+    xhr.send(formData);
   };
-
+  
   return (
     <div className="space-y-4">
       <FileInput 
