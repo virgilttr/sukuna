221a0977e648fb56b0ba102a86b07ff9aff2a514
virgil <virgil@virgils-mbp.mynetworksettings.com>
2024-08-27 22:34:02 -0400

    fix

--- frontend/components/uploader.tsx
+++ frontend/components/uploader.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useRef } from "react";
 import Link from "next/link";
-import PropertyDetails from "./PropertyDetails";
 
 type ExtractedInfo = {
   construction: string;
